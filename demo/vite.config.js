e40d7ace3f371d8a562f59fe943f22b309c43396
Ritvij Kumar Sharma <ritvij2001@gmail.com>
2026-01-10 17:31:01 +0400

    Fix base path for custom domain deployment

--- demo/vite.config.js
+++ demo/vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === "production" ? "/numis/" : "/",
+  base: "/",
   build: {
     outDir: "dist",
     emptyOutDir: true,
